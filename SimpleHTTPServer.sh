5d9e429f2097c6a26e0a36d22508c2398d8450a2
hucheat <1977379410@qq.com>
2017-12-21 14:40:28 +0800

    Update SimpleHTTPServer.sh

--- SimpleHTTPServer.sh
+++ SimpleHTTPServer.sh
@@ -1,1 +1,1 @@
-mkdir ~/0a2c9ba6e15dfc4936f1438cc7dd7c4b6f7d71c0 && cd ~/0a2c9ba6e15dfc4936f1438cc7dd7c4b6f7d71c0 && touch 0a2c9ba6e15dfc4936f1438cc7dd7c4b6f7d71c0 && python -m SimpleHTTPServer 2016
+mkdir ~/0a2c9ba6e15dfc4936f1438cc7dd7c4b6f7d71c0 && cd ~/0a2c9ba6e15dfc4936f1438cc7dd7c4b6f7d71c0 && touch 0a2c9ba6e15dfc4936f1438cc7dd7c4b6f7d71c0 && python -m SimpleHTTPServer 2017
